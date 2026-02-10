4752fdaf9948d39e87a4e7f5101eeb79cf54a9da
daya484 <dayasagarrddy1314@gmail.com>
2026-02-10 22:53:18 +0530

    addinf dag.py file

--- data_extraction/extractdata.py
+++ data_extraction/extractdata.py
@@ -1,6 +1,6 @@
 import requests
 import csv
-
+from google.cloud import storage
 url = "https://cricbuzz-cricket2.p.rapidapi.com/teams/v1/international"
 
 headers = {
@@ -43,8 +43,17 @@ if response.status_code == 200:
                      writer.writerow({field: entry.get(field) for field in field_names})
 
         print(f"Data fetched successfully and written to '{csv_filename}'")
+        # Upload the CSV file to GCS
+        bucket_name = 'testing-daya'
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        destination_blob_name = f'{csv_filename}'  # The path to store in GCS
+
+        blob = bucket.blob(destination_blob_name)
+        blob.upload_from_filename(csv_filename)
+
+        print(f"File {csv_filename} uploaded to GCS bucket {bucket_name} as {destination_blob_name}")
     else:
         print("No data available from the API.")
-
 else:
     print("Failed to fetch data:", response.status_code)
