3996fd67a2ca8241baae651291baf887fb414a9c
Daya484 <dayasagarreddy@gmail.com>
2026-02-15 11:18:40 +0000

    update deduplaction logic

--- DBT/models/Gold/stagging/stg_gold_name.sql
+++ DBT/models/Gold/stagging/stg_gold_name.sql
@@ -1,34 +1,33 @@
 with silver as(
 select 
-  market_code,
-  CASE
+market_code,
+CASE
     WHEN LENGTH(TRIM(market_code)) = 2 THEN TRIM(market_code)
     ELSE NULL
-  END AS market_code2,
-  CASE
+END AS market_code2,
+CASE
     WHEN LENGTH(TRIM(market_code)) = 3 THEN TRIM(market_code)
     ELSE NULL
-  END AS market_code3,
-team_id,	
+END AS market_code3,
+team_id,
 country_name,	
-image_id,	
+image_id,
 file_name,	
 update_timestamp
-
-from {{ source('src_team_ext','silver_team_distinct_ingest') }}
+from {{ ref('silver_team_distinct_ingest') }}
 )
 
 select 
 market_code,
-coalesce(s.market_code2,m.alpha2) as market_code2,
-coalesce(s.market_code3,m.alpha3) as market_code3,
+coalesce(m.alpha2,s.market_code2) as market_code2,
+coalesce(m.alpha3,s.market_code3) as market_code3,
 team_id,	
 country_name,	
 image_id,	
 file_name,	
 update_timestamp
 from silver as s
-left join {{ ref('dim_market') }} as m 
+left join {{ ref('dim_gold_market') }} as m 
 on lower(s.market_code2)=lower(m.alpha2)
 or lower(s.market_code3)=lower(m.alpha3)
 or lower(s.country_name)=lower(m.country)
